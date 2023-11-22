4d77627dfc2437ed1541e9cd5cb7b22cace16fcb
Jordan Manus <jlmanus17@gmail.com>
2023-11-22 16:39:47 -0500

    updated deck/urls to start the homepage at '' instead of decks.

--- deck/urls.py
+++ deck/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('decks', views.deck_list, name='home'),
-    path('decks/add', views.new_deck, name='new-deck'),
+    path('', views.deck_list, name='home'),
+    path('add', views.new_deck, name='new-deck'),
     # path('decks/<int:pk>', views.DeckDetailsView.as_view(), name='deck-details'),
-    path('decks/<int:pk>', views.card_details, name='deck-details'),
-    path('decks/<int:pk>/edit', views.edit_deck, name='edit-deck'),
-    path('decks/<int:pk>/delete', views.delete_deck, name='delete-deck'),
-    path('decks/<int:pk>/new_card', views.new_card, name='new-card'),
-    path('decks/<int:pk>/card<int:card_pk>/edit', views.edit_card, name='edit-card'),
-    path('decks/<int:pk>/card<int:card_pk>/delete', views.delete_card, name='delete-card'),
+    path('<int:pk>', views.card_details, name='deck-details'),
+    path('<int:pk>/edit', views.edit_deck, name='edit-deck'),
+    path('<int:pk>/delete', views.delete_deck, name='delete-deck'),
+    path('<int:pk>/new_card', views.new_card, name='new-card'),
+    path('<int:pk>/card<int:card_pk>/edit', views.edit_card, name='edit-card'),
+    path('<int:pk>/card<int:card_pk>/delete', views.delete_card, name='delete-card'),
 ]
