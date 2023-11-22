e9b24ee8dbd65d432256b2561444d01dea5d516c
Jordan Manus <jlmanus17@gmail.com>
2023-11-22 16:33:19 -0500

    added ability to toggle classes for question and answer portions of flashcards. allows better styling. need to update css.

--- static/main.js
+++ static/main.js
@@ -8,18 +8,31 @@ cards.forEach((card) => {
         console.log(card.classList);
         card.classList.toggle('is-flipped');
 
+        // allows me to add classes to question and answer elems
+        let question = card.childNodes[1].classList;
+        question.toggle('hide-question');
+        console.log('question: ', question);
+        let answer = card.childNodes[3].classList;
+        console.log('answer: ', answer);
+        answer.toggle('show-answer');
+        console.log('answer after toggle: ', answer);
 
-        console.log('event id', event.target.id)
-        let dataId = card.getAttribute('data-id')
+
+        // get access the congrats button to tally correct guesses
+        console.log('event id', event.target.id);
+        let dataId = card.getAttribute('data-id');
         let confirm_buttons = document.querySelectorAll("[data-id]");
         confirm_buttons.forEach((button) => {
+            let nodes = button.childNodes
+            console.log('button nodes: ', nodes)
             if (button.getAttribute('data-id') === dataId) {
                 button.classList.toggle('show');
             }
-        })
+        });
     })
 });
 
+// counts correct guesses once button is pushed
 let count = 0;
 let right_button_tally = document.querySelector('#right_button_tally')
 let right_buttons = document.querySelectorAll('.confirm-buttons')
@@ -32,5 +45,3 @@ right_buttons.forEach((button) => {
 
 
 });
-
-
